4aeebbbb3cb25e153964e86a4098ab5ccd365dd3
F_thx <1764362678@qq.com>
2025-06-03 12:48:41 +0800

    district_futa_maid_dispatch_center

--- src/registed/ngnl.ts
+++ src/registed/ngnl.ts
@@ -1,6 +1,6 @@
 import { useRing, useSimple } from "../planet-register";
 
 // ====ngnl 3479729949====
-useSimple('district_elf_lab', 'district_elf_nanoalloy', 'district_elf_patrol', 'district_elf_garden');
+useSimple('district_elf_lab', 'district_elf_nanoalloy', 'district_elf_patrol', 'district_elf_garden','district_futa_maid_dispatch_center');
 useRing('district_fe_fluegel_rw_city', 'district_fe_fluegel_rw_alloys', 'district_fe_fluegel_rw_goods', 'district_fe_fluegel_rw_research')
 useRing('district_exmachina_rw_city', 'district_exmachina_rw_alloys', 'district_exmachina_rw_goods')
