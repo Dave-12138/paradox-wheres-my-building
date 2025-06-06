b38b9df7da323898382abe2d1d9dd9174fd7e703
Dave_12138 <207955131@qq.com>
2025-06-06 15:47:56 +0800

    +architects of eden

---
+++ src/registed/aoe.ts
@@ -0,0 +1,7 @@
+import { useRing, useSimple } from "../planet-register";
+// ==== architects of eden 3489821671 ====
+useSimple("utopian_basefood_la","utopian_basefood_la","utopian_basefood_la","utopian_basefood_la");
+useSimple("utopian_baseminerals_la","utopian_baseminerals_la","utopian_baseminerals_la","utopian_baseminerals_la");
+useSimple("utopian_baseenergy_la","utopian_baseenergy_la","utopian_baseenergy_la","utopian_baseenergy_la");
+useSimple("utopian_basemilitary_la","utopian_basemilitary_la","utopian_basemilitary_la","utopian_basemilitary_la");
+useSimple("utopian_baseresort_la","utopian_baseresort_la","utopian_baseresort_la","utopian_baseresort_la");
